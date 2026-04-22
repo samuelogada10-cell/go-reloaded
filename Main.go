4ff76008e8df1a1dbfc78329aee28ed80eab5909
samuelogada10-cell <samuelogada10@gmail.com>
2026-04-22 20:20:13 +0100

    Create Main.go

---
+++ Main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	a := os.Args
+	if len(a) < 3 {
+		return
+	}
+	d, _ := os.ReadFile(a[1])
+
+	s := fixP(fixA(conv(string(d))))
+	os.WriteFile(a[2], []byte(s), 0644)
+}
