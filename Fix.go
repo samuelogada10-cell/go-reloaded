bbf5012362e4e7a3d72510e5c46608b3df7d9bc5
samuelogada10-cell <samuelogada10@gmail.com>
2026-04-23 05:07:33 +0100

    Refactor Fix.go to include fixP function
    Added fixP function to handle punctuation spacing.

--- Fix.go
+++ Fix.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"strings"
-)
+import "strings"
 
 func fixA(s string) string {
 	w := strings.Fields(s)
@@ -11,13 +9,11 @@ func fixA(s string) string {
 		if w[i] == "a" || w[i] == "A" {
 			next := strings.ToLower(w[i+1])
 
-			
 			if strings.ContainsAny(string(next[0]), "aeiou") {
 				w[i] += "n"
 				continue
 			}
 
-			
 			if strings.HasPrefix(next, "honest") ||
 				strings.HasPrefix(next, "hour") ||
 				strings.HasPrefix(next, "heir") ||
@@ -28,4 +24,11 @@ func fixA(s string) string {
 	}
 
 	return strings.Join(w, " ")
+}
+
+func fixP(s string) string {
+	for _, p := range []string{".", ",", "!", "?", ":", ";"} {
+		s = strings.ReplaceAll(s, " "+p, p)
+	}
+	return s
 }
