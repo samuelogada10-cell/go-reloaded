ab3da1143fa33480c5ee419d54728484093a3a34
samuelogada10-cell <samuelogada10@gmail.com>
2026-04-23 05:08:23 +0100

    Fix formatting and remove unnecessary lines in Main.go

--- Main.go
+++ Main.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-	"os"
-)
+import "os"
 
 func main() {
 	a := os.Args
 	if len(a) < 3 {
 		return
 	}
-	d, _ := os.ReadFile(a[1])
 
+	d, _ := os.ReadFile(a[1])
 	s := fixP(fixA(conv(string(d))))
 	os.WriteFile(a[2], []byte(s), 0644)
 }
